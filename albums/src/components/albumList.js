8e0222660eb78b791cb325fad4d027dce29dff76
Lucas Demoliner <lucas.demoliner@acad.pucrs.br>
2018-03-27 13:41:54 -0300

    Adicionando card e buscando dados da api

--- albums/src/components/albumList.js
+++ albums/src/components/albumList.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { View } from 'react-native'
 import axios from 'axios'
+import AlbumDetail from './albumDetail'
 
 class AlbumList extends Component{
 
+    state = { albums: [] };
+
     componentWillMount(){
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(JSON.stringfy(response)));
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums(){
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Lista</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
