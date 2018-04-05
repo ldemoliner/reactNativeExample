9d951b734ae309c5d517731501dc4aed325ae9dc
Lucas Demoliner <lucas.demoliner@acad.pucrs.br>
2018-04-05 14:49:43 -0300

    Adicionado firebase ao projeto

---
+++ manager/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component{
+    componentWillMount(){
+        const config = {
+            apiKey: 'AIzaSyCVn4I3YeLgN--aruyVrn3ORJ9A8W5Pkpc',
+            authDomain: 'manager-a1d63.firebaseapp.com',
+            databaseURL: 'https://manager-a1d63.firebaseio.com',
+            projectId: 'manager-a1d63',
+            storageBucket: 'manager-a1d63.appspot.com',
+            messagingSenderId: '502322683296'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello
+                    </Text>
+                </View>
+            </Provider>
+        )
+    }
+}
+
+export default App;
