b30c9b5c4285703891ecc6ee86a9bba4a7671dbd
Lucas Demoliner <lucas.demoliner@acad.pucrs.br>
2018-04-09 10:55:19 -0300

    Adicionado redux thunk ao projeto

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,11 +1,18 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED,
+         PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = { 
+    email: '',
+    password: ''
+ };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMAIL_CHANGED:
-             return { ...state, email: action.payload }; // PEGA TUDO QUE NEM EM STATE E TOCA ESSE EMAIL POR CIMA 
+             return { ...state, email: action.payload }; // PEGA TUDO QUE TEM EM STATE E TOCA ESSE EMAIL POR CIMA
+        case PASSWORD_CHANGED:
+             return { ...state, password: action.payload };
         default:
             return state;
     }
